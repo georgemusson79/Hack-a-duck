1a066461b21b9836903f423f9128b54b7cb95ef4
georgemusson79 <149239702+georgemusson79@users.noreply.github.com>
2024-11-01 22:09:52 +0000

    Update Texture.h

--- src/UserInterface/Texture.h
+++ src/UserInterface/Texture.h
@@ -1,1 +1,77 @@
 
+
+#ifndef TEXTUREHEADER_H
+#define TEXTUREHEADER_H
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include <iostream>
+
+#include "../MainGameplay/Vector2.h"
+#include "MainWindow.h"
+
+class Texture {
+   protected:
+    SDL_Texture* texture = nullptr;
+    std::string path;
+
+   public:
+    SDL_Rect* r = new SDL_Rect();
+    int rx;
+    int ry;
+    int rw;
+    int rh;
+    double rotation = 0;
+
+    void deleteTexture() {
+        SDL_DestroyTexture(texture);
+    }
+
+    Vector2 getCenter() {
+        return Vector2(r->x + (r->w / 2), r->y + (r->y / 2));
+    }
+
+    void setPath(std::string path) {
+        this->path = path;
+        this->deleteTexture();
+        auto s = IMG_Load(path.c_str());
+        this->texture = SDL_CreateTextureFromSurface(window->GetRenderer(), s);
+        if (!this->texture) std::cerr << "fucked\n";
+    }
+
+    std::string getPath() {
+        return this->path;
+    }
+    void setPath(std::string path) {
+        this->path = path;
+        this->deleteTexture();
+        auto s = IMG_Load(path.c_str());
+        this->texture = SDL_CreateTextureFromSurface(window->GetRenderer(), s);
+    }
+
+    Texture(const std::string& path, int x, int y, int w, int h) {
+        this->setPath(path);
+        r->x = x;
+        r->w = w;
+        r->h = h;
+        r->y = y;
+    }
+
+    void render() {
+        SDL_RenderCopy(window->GetRenderer(), this->texture, NULL, r);
+        // SDL_RenderCopyEx(renderer, texture, NULL, &dstrect, this->rotation, NULL, SDL_FLIP_NONE);
+    }
+
+    void render(int sx, int sy, int sw, int sh) {
+        SDL_Rect srcrect = {sx, sy, sw, sh};
+        SDL_RenderCopyEx(window->GetRenderer(), texture, &srcrect, r, this->rotation, NULL, SDL_FLIP_NONE);
+    }
+
+    ~Texture() {
+        this->deleteTexture();
+    }
+};
+
+#endif
+// esfionsfioshfoisfobisibfosboiufsbiuofbiuosEfbiuosfiobsbiuofdsbgeiu
