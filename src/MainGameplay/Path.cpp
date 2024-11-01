1144700721269980541f539db4b9029e458b8447
georgemusson79@gmail.com <georgemusson79@gmail.com>
2024-11-01 23:55:38 +0000

    path update

--- src/MainGameplay/Path.cpp
+++ src/MainGameplay/Path.cpp
@@ -1,10 +1,13 @@
 #include "Path.h"
 
 Node::Node(Vector2 _nodePos, Node* _lastNode, bool isLast) {
-	nodePos = _nodePos;
-	isFirstNode = (_lastNode == nullptr);
-	isLastNode = isLast; 
-	if (_lastNode != nullptr) _lastNode->nextNode = this;
+    nodePos = _nodePos;
+    isFirstNode = (_lastNode == nullptr);
+    isLastNode = isLast;
+    if (_lastNode != nullptr) _lastNode->nextNode = this;
+    double rotation = rand() % 360;
+    std::vector<std::string> pngs = {"pad.png", "lilypad.png", "cluster.png"};
+    std::string png = "resources/" + pngs[rand() % 3];
+    t = Texture(png, this->nodePos.x, this->nodePos.y, this->w, this->h);
+    t.rotation = rotation;
 }
-
-
--- src/MainGameplay/Path.h
+++ src/MainGameplay/Path.h
@@ -6,22 +6,27 @@
 #include <memory>
 #include <vector>
 
+#include "../UserInterface/Texture.h"
 #include "Vector2.h"
 
 // Each Tile is 20x20, making a 50x50 grid.
 class Node {
    private:
+    Texture t;
     Vector2 nodePos;
     Node* nextNode;
     bool isLastNode;
     bool isFirstNode;
+    int w = 800 / 20;
+    int h = 800 / 20;
 
    public:
     Node(Vector2 _nodePos, Node* _lastNode, bool isLast = false);
     [[nodiscard]] Node* GetNextNode() const { return nextNode; };
     [[nodiscard]] Vector2 GetPosition() const { return nodePos; };
     void display() {
-        }
+        this->t.render();
+    }
 };
 
 inline std::vector<std::unique_ptr<Node>> pathNodes;
