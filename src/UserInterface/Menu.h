20ae1e3f6666bc6d8bdf40565f7e4557f1ab2cbd
Iris <ultimatetuber22@gmail.com>
2024-11-01 18:45:50 +0000

    Created Menu header and cpp files

---
+++ src/UserInterface/Menu.h
@@ -0,0 +1,23 @@
+#ifndef HACK_A_DUCK_MENU_H
+#define HACK_A_DUCK_MENU_H
+
+#include <SDL.h>
+#include <memory>
+
+class Menu {
+	private:
+		SDL_Window* window {};
+    	SDL_Renderer* renderer {};
+
+    public:
+        Menu();
+
+        void Display();
+
+        [[nodiscard]] SDL_Window* GetWindow() const { return window; }
+        [[nodiscard]] SDL_Renderer* GetRenderer() const { return renderer; }
+}
+
+inline std::unique_ptr<Menu> window;
+
+#endif //HACK_A_DUCK_MENU_H
