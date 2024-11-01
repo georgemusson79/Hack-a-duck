0d75e6ba748be1a8a1ab00354b299cf2a24875a2
MisterPuggs <callum.wilkie2023@my.ntu.ac.uk>
2024-11-01 16:22:36 +0000

    setup

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by cew05 on 01/11/2024.
+//
+
+#define SDL_MAIN_HANDLED
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+    printf("this is doomed");
+
+    return 0;
+}
