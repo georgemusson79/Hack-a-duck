b7ae59f839efc9d7d926b080f04a5c30ad01cd99
Iris <ultimatetuber22@gmail.com>
2024-11-01 20:29:05 +0000

    Updated Path.h

--- src/MainGameplay/Path.h
+++ src/MainGameplay/Path.h
@@ -1,11 +1,26 @@
+<<<<<<< HEAD
 #include <../MainGameplay/Cat.h>
 #include <../UserInterface/Texture.h>
 
 #include "Vector2.h"
+=======
+#include "Vector2.h"
+#include "../UserInterface/Texture.h"
+#include "../UserInterface/Cat.h"
+>>>>>>> cced3c2 (Updated Path.h)
 
+// Each Tile is 20x20, making a 50x50 grid.
 class Path {
+<<<<<<< HEAD
     Vector2 currentTilePos;
     Path* nextTile;
     bool isLastTile;
     Path();
+=======
+	Vector2 tilePos;
+	bool isLastTile;
+	Path *nextPath;
+
+	Path();
+>>>>>>> cced3c2 (Updated Path.h)
 }
--- src/MainGameplay/Vector2.h
+++ src/MainGameplay/Vector2.h
@@ -1,6 +1,8 @@
 #ifndef EB8F59A9_5045_40A5_B52E_488E8E3ED531
 #define EB8F59A9_5045_40A5_B52E_488E8E3ED531
 
+// Need to multiply x and y by 50 to make it the full h/w of 1k x 1k
+
 class Vector2 {
    public:
     int x;
