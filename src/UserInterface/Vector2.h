ef17b4f84f52551c2a06291bac1d382e0dace4ef
georgemusson79@gmail.com <georgemusson79@gmail.com>
2024-11-01 20:02:35 +0000

    vectors

---
+++ src/UserInterface/Vector2.h
@@ -0,0 +1,15 @@
+#ifndef EB8F59A9_5045_40A5_B52E_488E8E3ED531
+#define EB8F59A9_5045_40A5_B52E_488E8E3ED531
+
+class Vector2 {
+   public:
+    int x;
+    int y;
+
+    Vector2(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+}
+
+#endif /* EB8F59A9_5045_40A5_B52E_488E8E3ED531 */
